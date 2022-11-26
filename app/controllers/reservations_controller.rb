2ac910b7479718b7ec23c3481b3f5a1125cc2b77
yhuzued <yusufmait@gmail.com>
2022-11-26 11:32:36 +0700

    Creating reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,26 +1,23 @@
 class ReservationsController < ApplicationController
-  
+  before_action :authenticate_user!, only: %i[ new create ]
+
   def index
     @event = Event.all
   end
 
   def new
     @event = Event.find(params[:event_id])
-    @reservation = @event.reservations.build(event_id: params[:id], attendee_id: current_user.id)
+    @reservation = @event.reservations.build(attendee_id: current_user.id)
   end
 
   def create
     @event = Event.find(params[:event_id])
     @reservation = @event.reservations.build(event_id: params[:id], attendee_id: current_user.id)
 
-    respond_to do |format|
-      if @event.save
-        format.html { render :show, notice: "Reservations was successfully created." }
-        format.json { render :show, status: :created, location: @event }
-      else
-        format.html { render :show, status: :unprocessable_entity }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
+    if @reservation.save
+      redirect_to @event, notice: "Reservations was successfully created."
+    else
+      render @event, status: :unprocessable_entity
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :events do
-    resources :reservations, only: [:new]
+    resources :reservations, only: [:new, :create]
   end
   root "events#index"
 end
